01615bc12d542f7e4a26050012b3ec3d00b3d0e9
cweibel73 <carleweibel@gmail.com>
2020-03-09 18:53:02 -0700

    Updates

---
+++ precache-manifest.d2302f87ad77ea0774da9af379a4b870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8e77f761550aca09aa835c2322a08c",
+    "url": "/triviapage/index.html"
+  },
+  {
+    "revision": "3b10d7ed359fa8be18cf",
+    "url": "/triviapage/static/css/main.9821f248.chunk.css"
+  },
+  {
+    "revision": "3100a7868ba37dd7beeb",
+    "url": "/triviapage/static/js/2.58c30fa1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/triviapage/static/js/2.58c30fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b10d7ed359fa8be18cf",
+    "url": "/triviapage/static/js/main.551df168.chunk.js"
+  },
+  {
+    "revision": "bb5778abb89d07c71475",
+    "url": "/triviapage/static/js/runtime-main.66a20295.js"
+  }
+]);
